f4f4c4198fcf5b5ac2a832aed8a496e46bc3b4f6
leon-wee <leonwee27@gmail.com>
2015-09-12 23:27:56 +0700

    Added test for adding task

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,6 +1,20 @@
 describe('To Do List', function() {
-  it('has a title', function() {
+
+  var inputBox = element(by.model('toDoCtrl.inputTask'))
+  var addTaskButton = element(by.className('btn'))
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('To do list')
+  });
+
+  it('shows the number of counts on the list', function() {
+    inputBox.sendKeys('Buy some toiletries');
+    addTaskButton.click();
+    var tasks = element.all(by.repeater('task in toDoCtrl.allTasks'))
+    expect(tasks.getText()).toEqual(['Buy some toiletries'])
   });
 });

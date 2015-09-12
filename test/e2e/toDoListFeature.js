cdbda7648a341d6242c751c7a474ae71e34bf618
leon-wee <leonwee27@gmail.com>
2015-09-13 00:12:07 +0700

    Added feature test for delete

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,7 +1,9 @@
 describe('To Do List', function() {
 
   var inputBox = element(by.model('toDoCtrl.inputTask'))
-  var addTaskButton = element(by.className('btn'))
+  var addTaskButton = element(by.className('add-btn'))
+  var taskList = element.all(by.css('li'))
+  var deleteList = element.all(by.className('delete-btn'))
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
@@ -11,11 +13,12 @@ describe('To Do List', function() {
     expect(browser.getTitle()).toEqual('To do list')
   });
 
-  it('shows the task when it is added', function() {
+  it('shows the tasks when it is added', function() {
     inputBox.sendKeys('Buy some toiletries');
     addTaskButton.click();
-    var tasks = element.all(by.repeater('task in toDoCtrl.allTasks'))
-    expect(tasks.getText()).toEqual(['Buy some toiletries'])
+    inputBox.sendKeys('Watch some TV')
+    addTaskButton.click();
+    expect(taskList.get(0).getText()).toEqual('Buy some toiletries')
   });
 
   it('shows the total number of tasks', function() {
@@ -25,6 +28,16 @@ describe('To Do List', function() {
     addTaskButton.click();
     var totalCount = element(by.className('total-count'))
     expect(totalCount.getText()).toEqual('2')
+  });
+
+  it('can delete the tasks on the front-end', function() {
+    inputBox.sendKeys('test1');
+    addTaskButton.click();
+    inputBox.sendKeys('test2');
+    addTaskButton.click();
+    deleteList.get(0).click()
+    var totalCount = element(by.className('total-count'))
+    expect(totalCount.getText()).toEqual('1')
   });
 
 });

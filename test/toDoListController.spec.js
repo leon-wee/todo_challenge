c5cf2d77cbf06ea6a73220d2d92bc0a001bdaa48
leon-wee <leonwee27@gmail.com>
2015-09-13 18:08:20 +0700

    Refactored unit testing

--- test/toDoListController.spec.js
+++ test/toDoListController.spec.js
@@ -12,7 +12,7 @@ describe('ToDoListController', function() {
     expect(ctrl.inputTask).toBeUndefined();
   });
 
-  describe('tasks', function() {
+  describe('storing tasks', function() {
     beforeEach(function() {
       ctrl.inputTask = "Do my laundry"
       ctrl.addTask()
@@ -27,22 +27,35 @@ describe('ToDoListController', function() {
       ctrl.addTask()
       expect(ctrl.allTasks).toEqual([{ description: 'Do my laundry', editing: false, completed: 'active'}])
     });
+  });
+
+  describe('editing task status', function() {
+    beforeEach(function() {
+      ctrl.inputTask = "Do my laundry"
+      ctrl.addTask()
+    });
 
     it('edit tasks status changes to true', function() {
       ctrl.editTask(ctrl.allTasks[0])
       expect(ctrl.allTasks[0].editing).toEqual(true)
     });
+  });
 
-    it('can delete task on the list', function() {
+  describe('deleting tasks', function() {
+    beforeEach(function() {
+      ctrl.inputTask = "Do my laundry"
+      ctrl.addTask()
+    });
+
+    it('can delete specific task on the list', function() {
       ctrl.inputTask = "Learning Sinatra"
       ctrl.addTask()
       // can't solve the indexOf method on karma but it works on feature tests!!!
       ctrl.allTasks.splice(0, 1)
       expect(ctrl.allTasks).toEqual([{ description: 'Learning Sinatra', editing: false, completed: 'active' }])
     });
+  })
 
-
-  });
 
 
 
